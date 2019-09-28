92d8c7eae7624e0cebf74543c6fc497f26d8f9ef
Akshay B <akshay00as11@gmail.com>
2019-09-28 13:51:37 +0530

    Add files via upload
    First uploads

---
+++ quadRX/quadRX.ino
@@ -0,0 +1,54 @@
+#include <ServoTimer2.h>
+#include <VirtualWire.h>
+
+ServoTimer2 servo1;
+ServoTimer2 servo2;
+ServoTimer2 servo3;
+ServoTimer2 servo4;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    vw_set_ptt_inverted(true);
+    vw_set_rx_pin(3);
+    vw_set_ptt_pin(4);
+    vw_setup(2000);
+    vw_rx_start();
+
+    servo1.attach(9);
+    servo2.attach(10);
+    servo3.attach(11);
+    servo4.attach(12);
+    pinMode(26,OUTPUT);
+    digitalWrite(26,LOW);
+}
+
+uint8_t val[10];
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen))
+  {
+	  for (uint8_t i = 0; i < buflen; i++)
+	  {
+      val[i] = buf[i];
+	  }
+  }
+  servo1.write(map(val[2],0,255,1000,2000));
+  servo2.write(map(val[2],0,255,1000,2000));
+  servo3.write(map(val[2],0,255,1000,2000));
+  servo4.write(map(val[2],0,255,1000,2000));
+
+  Serial.print(val[0]);
+  Serial.print("  ");
+  Serial.print(val[1]);
+  Serial.print("  ");
+  Serial.print(val[2]);
+  Serial.print("  ");
+  Serial.print(val[3]);
+  Serial.println();
+}
---
+++ quadTX/quadTX.ino
@@ -0,0 +1,29 @@
+#include <VirtualWire.h>
+
+void setup() {
+  //Serial.begin(9600);
+  for(int i=5;i<13;i++)
+    pinMode(i, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+  vw_set_tx_pin(4);
+  vw_set_ptt_pin(13);
+}
+
+int a[13];
+void loop() {
+  uint8_t sen1 = map(analogRead(A0), 0, 1023, 0, 255);
+  uint8_t sen2 = map(analogRead(A1), 0, 1023, 0, 255);
+  uint8_t sen3 = map(analogRead(A2), 0, 1023, 0, 255);
+  uint8_t sen4 = map(analogRead(A3), 0, 1023, 0, 255);
+  uint8_t sw1 = digitalRead(2);
+
+  char msg[6] = {sen1,sen2,sen3,sen4,sw1}; 
+
+  vw_send((uint8_t *)msg,5);
+  vw_wait_tx();
+  delay(50);
+}
